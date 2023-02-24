441b6d1890a454b3d709ddf4b094235c8ea1df6b
atvnbi <54908056+atvnbi@users.noreply.github.com>
2023-02-24 13:18:13 +0100

    Add variables practice

--- foundations/js-foundation/js-basics-01/javascript.js
+++ foundations/js-foundation/js-basics-01/javascript.js
@@ -1,1 +1,29 @@
 console.log("Test console.log");
+
+// working with variables
+let admin;
+
+let name = "John";
+
+admin = name;
+
+alert(admin);
+
+// giving the right name
+let ourPlanetName = "Earth"
+
+let currentUserName = "John"
+
+console.log(ourPlanetName);
+console.log(currentUserName);
+
+// constants - uppercase constants
+const COLOR_RED = "#F00" // Capital-named constants are only used as aliases for “hard-coded” values.
+
+const BIRTHDAY = '18.10.2001' // In this code, birthday is exactly like that. So we could use the upper case for it.
+
+const age = (BIRTHDAY); // age is evaluated in run-time. Today we have one age, a year after we’ll have another one. It is constant in a sense that it does not change through the code execution. But it is a bit “less of a constant” than birthday: it is calculated, so we should keep the lower case for it.
+
+console.log(COLOR_RED);
+console.log(BIRTHDAY);
+console.log(age);
